8010d928c92319a7236d8cd1b9edfe580fe7ed04
worldOneo <62426504+worldOneo@users.noreply.github.com>
2022-01-23 22:02:21 +0100

    Added chart to trace

--- pages/dashboard/trace/[id].tsx
+++ pages/dashboard/trace/[id].tsx
@@ -14,39 +14,64 @@ import {
   Tr,
   Th,
   Tbody,
-  Td
+  Td,
+  useColorMode
 } from "@chakra-ui/react";
 import { Stats } from "@prisma/client";
 import { useStats, useTraces } from "client/client";
 import Layout from "components/dashboard/layout";
+import dynamic from "next/dynamic";
 import { useRouter } from "next/router";
-import { useEffect } from "react";
-import { QuarterMinute, seedDate, unitOrder } from "store/units";
+import { useEffect, useMemo } from "react";
+import { seedDate, unitOrder } from "store/units";
 import { nTon } from "utils/utils";
+const ReactApexChart = dynamic(() => import("react-apexcharts"), { ssr: false });
 
 function StatsOf({stats, unit}: {stats: Stats[], unit: number}) {
-  return <Table>
-    <Thead>
-      <Tr>
-        <Th>Date</Th>
-        <Th>Occurrences</Th>
-      </Tr>
-    </Thead>
-    <Tbody>
-      {
-        stats
-          .filter(s => nTon(s.unit) === unit)
-          .sort((a, b) => nTon(b.bucket) - nTon(a.bucket))
-          .map(s =><Tr key={nTon(s.bucket)}>
+  const {colorMode} = useColorMode();
+
+  const filtered = useMemo(() => stats
+    .filter(s => nTon(s.unit) === unit)
+    .sort((a, b) => nTon(b.bucket) - nTon(a.bucket)), [stats, unit]);
+
+  const timeSeries = useMemo(() => filtered
+    .map(s => [(nTon(s.bucket)*unit + seedDate), s.hits] as [number, number]),
+  [filtered, unit]);
+
+  return <VStack>
+    <ReactApexChart height={350} width={600} options={{
+      theme: {
+        mode: colorMode
+      },
+      xaxis: {
+        type: "datetime"
+      }
+    }} series={[{
+      name: "Occurences",
+      data: timeSeries
+    }]}/>
+    <Table>
+      <Thead>
+        <Tr>
+          <Th>From</Th>
+          <Th>To</Th>
+          <Th>Occurrences</Th>
+        </Tr>
+      </Thead>
+      <Tbody>
+        {
+          filtered.map(s =><Tr key={nTon(s.bucket)}>
             <Td>
               {new Date(nTon(s.bucket)*unit + seedDate).toLocaleString()}
-              {" - "}
+            </Td>
+            <Td>
               {new Date((nTon(s.bucket)+1)*unit + seedDate).toLocaleString()}</Td>
             <Td>{s.hits}</Td>
           </Tr>)
-      }
-    </Tbody>
-  </Table>;
+        }
+      </Tbody>
+    </Table>
+  </VStack>;
 }
 
 export default function TraceDetail() {
@@ -55,6 +80,9 @@ export default function TraceDetail() {
   const id = router.query["id"] as string;
   const trace = data?.find(t => t.id === id);
   const [stats, fetchStats] = useStats(id);
+  const statPanels = useMemo(() => stats?unitOrder.map(u => <TabPanel key={u}>
+    <StatsOf stats={stats} unit={u}/>
+  </TabPanel>)  : null, [stats]);
 
   useEffect(() => {
     fetchStats();
@@ -69,7 +97,7 @@ export default function TraceDetail() {
   return <Layout>
     <Center>
       <VStack maxW="100%">
-        <Heading>Trace for &quot;{trace.event}&quot; in {trace.application} </Heading>
+        <Heading>Trace &quot;{trace.event}&quot; in {trace.application} </Heading>
         <Code whiteSpace="pre" overflow="auto" maxW="100%">
           {trace.trace}
         </Code>
@@ -86,10 +114,7 @@ export default function TraceDetail() {
               </TabList>
 
               <TabPanels>
-                {unitOrder.map(u => <TabPanel key={u}>
-                  <StatsOf stats={stats} unit={u}/>
-                </TabPanel>
-                )}
+                {statPanels}
               </TabPanels>
             </Tabs>
           </Box> : null}
